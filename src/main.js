a8c84796998fa7fa333bb5c5fcae552cc70d666d
Andrew-McCall <amccall@qa.com>
2022-07-06 20:25:37 +0100

    Buggy A*

--- src/main.js
+++ src/main.js
@@ -5,90 +5,151 @@ const clear = document.getElementById("clear");
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const SIZE = 50;
-const SCALE = 10;
+const SIZE = 5;
+const SCALE = 100;
 
-canvas.height = SIZE*SCALE
-canvas.width = SIZE*SCALE
+canvas.height = SIZE * SCALE
+canvas.width = SIZE * SCALE
 
-let data = new Array(SIZE*SIZE).fill(0);
+let data = new Array(SIZE * SIZE).fill(0);
 
-function readData(x, y){
-    return data[(x%SIZE) + (y%SIZE)*SIZE];
+function readData(x, y) {
+    return data[(x % SIZE) + (y % SIZE) * SIZE];
 }
-function writeData(x, y, value){
-    data[(x%SIZE) + (y%SIZE)*SIZE] = value;
+function writeData(x, y, value) {
+    data[(x % SIZE) + (y % SIZE) * SIZE] = value;
 }
 
-function calculate(){
+function calculate() {
+    function surrounding(coords) {
+        const current = distance[coords.x + coords.y * SIZE]
+        for (let dx = -1; dx < 2; dx++){
+            for (let dy = -1; dy < 2; dy++) {
+                if (dx === 0 && dy === 0) continue;
+                const cx = coords.x+dx;
+                const cy = coords.y+dy;
+                if (cx >= 0 &&  cx < SIZE && cy >= 0 && cy < SIZE){
+                    
+                    if (null === distance[cx + cy * SIZE]){
+                        distance[cx + cy * SIZE] = {to:Math.abs(goal.x-cx) + Math.abs(goal.y-cy),from:current.from+1,path:coords, x:cx, y:cy}
+                    }else if (distance[cx + cy * SIZE].from > current.from+1){
+                        distance[cx + cy * SIZE].from = current.from+1;
+                        distance[cx + cy * SIZE].path = coords;
+                    }
+                }
+            }
+        }   
+    }
+    let distance = new Array(SIZE * SIZE).fill(null);
+
     let start = null;
     let goal = null;
-    for (let x = 0; x < SIZE; x++){
-        for (let y = 0; y < SIZE; y++){
-            const current = readData(x,y);
+    for (let x = 0; x < SIZE; x++) {
+        for (let y = 0; y < SIZE; y++) {
+            const current = readData(x, y);
             if (current === 2) {
-                start = {x,y}
-                if (goal != null){
+                start = { x, y }
+                if (goal != null) {
                     break;
                 }
             }
-            else if(current == 3) {
-                goal = {x,y}
-                if (start != null){
+            else if (current == 3) {
+                goal = { x, y }
+                if (start != null) {
                     break;
                 }
             }
         }
     }
 
-    if (!start || !goal){
+    if (!start || !goal) {
         console.log("TODO: Error for no start/goal")
-    }else{
-        
+    } else {
+        distance[start.x + start.y * SIZE] = {to:Math.abs(start.x-goal.x) + Math.abs(start.y-goal.x),from:0,path:{x:start.x, y:start.y}, x:start.x, y:start.y}
+
+        let lowest = distance[start.x + start.y * SIZE];
+        while (lowest.to !== 0){
+            surrounding(lowest, distance)
+            let lowestDistance = lowest.to + lowest.to + lowest.from
+            for (let x = 0; x < SIZE; x++) {
+                for (let y = 0; y < SIZE; y++) {
+                    const current = distance[x + y * SIZE]
+                    if (current){
+                        const currentDistance = current.to + current.to + current.from
+                        if (lowestDistance > currentDistance){
+                            lowestDistance = currentDistance;
+                            lowest = current;
+                        }
+                    }
+                }
+            }
+        }
+
+        while (lowest.from !== 0){
+            ctx.fillStyle = 'pink';
+            ctx.fillRect(lowest.x * SCALE, lowest.y * SCALE, SCALE, SCALE);
+            console.log(lowest.x , lowest.y)
+            for (let dx = -1; dx < 2; dx++){
+                for (let dy = -1; dy < 2; dy++) {
+                    if (dx === 0 && dy === 0) continue;
+                    const cx = lowest.x+dx;
+                    const cy = lowest.y+dy;
+                    if (cx >= 0 &&  cx < SIZE && cy >= 0 && cy < SIZE){
+                        const current = distance[cx + cy * SIZE]
+                        if (current && current.from < lowest.from){
+                            lowest = current;
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+
     }
-    
-
-
 
 }
 
-function updateCanvas(){
-    for (let x = 0; x < SIZE; x++){
-        for (let y = 0; y < SIZE; y++){
-            switch (readData(x,y)){
+function updateCanvas() {
+    for (let x = 0; x < SIZE; x++) {
+        for (let y = 0; y < SIZE; y++) {
+            switch (readData(x, y)) {
                 case 0:
-                    ctx.fillStyle = 'white'; 
+                    ctx.fillStyle = 'white';
                     break;
                 case 1:
-                    ctx.fillStyle = 'black'; 
+                    ctx.fillStyle = 'black';
                     break;
                 case 2:
                     ctx.fillStyle = 'green';
                     break;
                 case 3:
-                    ctx.fillStyle = 'red'; 
+                    ctx.fillStyle = 'red';
                     break;
                 case 5:
-                    ctx.fillStyle = 'pink'; 
+                    ctx.fillStyle = 'pink';
                     break;
             }
             ctx.fillRect(x * SCALE, y * SCALE, SCALE, SCALE);
         }
     }
-    calculate();
 }
 updateCanvas()
 
+playButton.onclick = (e) => {
+    calculate();
+}
+
 clear.onclick = (e) => {
-    data = new Array(SIZE*SIZE).fill(0);
+    data = new Array(SIZE * SIZE).fill(0);
 }
 
 let pen = 0;
 let isMouseDown = false;
 editor[0].disabled = true;
 
-for (let i = 0; i<editor.length; i++){
-    editor[i].onclick = (e) =>{
+for (let i = 0; i < editor.length; i++) {
+    editor[i].onclick = (e) => {
         editor[pen].disabled = false;
         editor[i].disabled = true;
         pen = i;
@@ -99,9 +160,9 @@ const rect = canvas.getBoundingClientRect()
 
 canvas.addEventListener('mousedown', (e) => {
     isMouseDown = true;
-    const x = Math.floor((e.x - rect.left)/SCALE)
-    const y = Math.floor((e.y - rect.top)/SCALE)
-    if (pen === 2 || pen === 3){
+    const x = Math.floor((e.x - rect.left) / SCALE)
+    const y = Math.floor((e.y - rect.top) / SCALE)
+    if (pen === 2 || pen === 3) {
         deleteType(pen)
     }
     writeData(x, y, pen);
@@ -112,14 +173,14 @@ canvas.addEventListener("mouseup", (e) => {
     isMouseDown = false;
 })
 
-canvas.addEventListener("mousemove", (e) =>{
-    if (isMouseDown){
-        const x = Math.floor((e.x - rect.left)/SCALE)
-        const y = Math.floor((e.y - rect.top)/SCALE)
-        if (x > SIZE || x < 0 || y > SIZE || y < 0){
+canvas.addEventListener("mousemove", (e) => {
+    if (isMouseDown) {
+        const x = Math.floor((e.x - rect.left) / SCALE)
+        const y = Math.floor((e.y - rect.top) / SCALE)
+        if (x > SIZE || x < 0 || y > SIZE || y < 0) {
             return;
         }
-        if (pen === 2 || pen === 3){
+        if (pen === 2 || pen === 3) {
             deleteType(pen)
         }
         writeData(x, y, pen);
@@ -127,11 +188,11 @@ canvas.addEventListener("mousemove", (e) =>{
     }
 })
 
-function deleteType(value){
-    for (let x = 0; x < SIZE; x++){
-        for (let y = 0; y < SIZE; y++){
-            if (readData(x,y) === value) {
-                writeData(x,y, 0)
+function deleteType(value) {
+    for (let x = 0; x < SIZE; x++) {
+        for (let y = 0; y < SIZE; y++) {
+            if (readData(x, y) === value) {
+                writeData(x, y, 0)
             }
         }
     }
