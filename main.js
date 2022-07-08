a45bb3e5ec49d4a5951c6691dcbe5d388d69a7c1
Andrew-McCall <amccall@qa.com>
2022-07-08 18:05:44 +0100

    Resized App

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron');
 app.whenReady().then( () => {
 
     const mainWindow = new BrowserWindow({
-        width:532,
+        width:516,
         height:626,
         resizable:false,
         autoHideMenuBar: true,
